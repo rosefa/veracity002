ee24a4ccc415e603e0c29b37aca8984c60c9a2bc
rosefa <82177268+rosefa@users.noreply.github.com>
2023-08-29 12:43:44 +0000

    Update myClass.py

--- myClass.py
+++ myClass.py
@@ -37,8 +37,12 @@ class FakeMultimodal:
       # Learning control params
       self.epochs = epochs
       self.batch_size = batch_size
-      self.models = []
-      self.scores = {}
+      self.best_models = []
+      self.result = []
+      self.scores_loss = []
+      self.scores_acc = []
+      self.scores_pre = []
+      self.scores_rap = []
     
   def bert_encode(self, texts):
     all_tokens = []
@@ -88,79 +92,126 @@ def fake_virtual(self):
   return final_model
 
 def train(self, X):
-  result = []
-  scores_loss = []
-  scores_acc = []
-  scores_pre = []
-  scores_rap = []
-  fold_var = 1
-  for train_indices, val_indices in kf.split(self.X):
-    print(fold_var)
-    train = self.X.iloc[train_indices]
-    val = self.X.iloc[val_indices]
-    label = preprocessing.LabelEncoder()
-    
-    trainText = self.bert_encode(train['text'], tokenizer, max_len=max_len)
-    trainImage = train['image']
-    trainImage = trainImage.to_numpy()
-    # Conversion
-    trainImage = np.array([val for val in trainImage])
-    trainLabel = label.fit_transform(train['label'])
-    trainLabel = to_categorical(trainLabel)
-    labels = label.classes_
-    # print(labels)
-    # print(trainLabel)
-    valText = self.bert_encode(val['text'], tokenizer, max_len=max_len)
-    valImage = val['image']
-    valImage = valImage.to_numpy()
-    valImage = np.array([val for val in valImage])
-  
-    valLabel = label.fit_transform(val['label'])
-    valLabel = to_categorical(valLabel)
-    valLabel = valLabel
-  
-    testText = bert_encode(dataImageTextTest['text'], tokenizer, max_len=max_len)
-    testImage = dataImageTextTest['image']
+    fold_var = 1
+    for train_indices, val_indices in kf.split(self.X):
+        print(fold_var)
+        train = self.X.iloc[train_indices]
+        val = self.X.iloc[val_indices]
+        label = preprocessing.LabelEncoder()
+        
+        trainText = self.bert_encode(train['text'])
+        trainImage = train['image']
+        trainImage = trainImage.to_numpy()
+        # Conversion
+        trainImage = np.array([val for val in trainImage])
+        trainLabel = label.fit_transform(train['label'])
+        trainLabel = to_categorical(trainLabel)
+        labels = label.classes_
+        # print(labels)
+        # print(trainLabel)
+        valText = self.bert_encode(val['text'])
+        valImage = val['image']
+        valImage = valImage.to_numpy()
+        valImage = np.array([val for val in valImage])
+        
+        valLabel = label.fit_transform(val['label'])
+        valLabel = to_categorical(valLabel)
+        valLabel = valLabel
+        
+        #testText = self.bert_encode(dataImageTextTest['text'])
+        #testImage = dataImageTextTest['image']
+        #testImage = testImage.to_numpy()
+        #testImage = np.array([val for val in testImage])
+        
+        #testLabel = label.fit_transform(dataImageTextTest['label'])
+        #testLabel = to_categorical(testLabel)
+        #testLabel = testLabel
+        
+        file_path = save_dir+'model_'+str(fold_var)+".hdf5"
+        checkpoint = tf.keras.callbacks.ModelCheckpoint(file_path, monitor='loss', verbose=0, save_best_only=True, mode='min')
+        earlystopping = tf.keras.callbacks.EarlyStopping(monitor="loss", mode="min", patience=8)
+        callbacks_list = [checkpoint,earlystopping]
+        #print(type(trainText),type(trainImage),type(trainLabel))
+        
+        hist = model.fit(x=[trainText,trainImage],y=trainLabel,epochs=30,batch_size=70,validation_data=([valText, valImage], valLabel), callbacks=callbacks_list, verbose=1)
+        model.load_weights(file_path)
+        score = model.evaluate([valText, valImage],valLabel, verbose=0)
+        self.scores_loss.append(score[0])
+        self.scores_acc.append(score[1])
+        self.scores_pre.append(score[2])
+        self.scores_rap.append(score[3])
+        #self.result.append(model.predict([testText,testImage]))
+        '''
+        accuracy = history.history['accuracy']
+        val_accuracy = history.history['val_accuracy']
+        epochs = range(1, len(accuracy) + 1)
+        plot_accuracy(epochs, accuracy,val_accuracy,fold_var)
+        
+        loss = history.history['loss']
+        val_loss = history.history['val_loss']
+        epochs = range(1, len(loss) + 1)
+        plot_loss(epochs, loss,val_loss,fold_var)
+        
+        fper, tper, thresholds = roc_curve(testLabel, y_pred)
+        plot_roc_curve(fper, tper,fold_var)
+        '''
+        tf.keras.backend.clear_session()
+        fold_var += 1
+    value_min = min(self.scores_loss)
+    value_index = self.scores_loss.index(value_min)
+    model.load_weights(save_dir+'model_'+str(value_index)+".hdf5")
+    best_model = model
+    self.best_models.append(best_model)
+    best_model.save_weights('model_weights.h5')
+    best_model.save('model_keras.h5')
+
+def predictModel(self, X):
+    testText = self.bert_encode(X['text'])
+    testImage = X['image']
     testImage = testImage.to_numpy()
     testImage = np.array([val for val in testImage])
-  
-    testLabel = label.fit_transform(dataImageTextTest['label'])
+    testLabel = label.fit_transform(X['label'])
     testLabel = to_categorical(testLabel)
     testLabel = testLabel
-    
-    file_path = save_dir+'model_'+str(fold_var)+".hdf5"
-    checkpoint = tf.keras.callbacks.ModelCheckpoint(file_path, monitor='loss', verbose=0, save_best_only=True, mode='min')
-    earlystopping = tf.keras.callbacks.EarlyStopping(monitor="loss", mode="min", patience=8)
-    callbacks_list = [checkpoint,earlystopping]
-    #print(type(trainText),type(trainImage),type(trainLabel))
-  
-    hist = model.fit(x=[trainText,trainImage],y=trainLabel,epochs=30,batch_size=70,validation_data=([valText, valImage], valLabel), callbacks=callbacks_list, verbose=1)
-    model.load_weights(file_path)
-    score = model.evaluate([valText, valImage],valLabel, verbose=0)
-    scores_loss.append(score[0])
-    scores_acc.append(score[1])
-    scores_pre.append(score[2])
-    scores_rap.append(score[3])
-    result.append(model.predict([testText,testImage]))
-    '''
-    accuracy = history.history['accuracy']
-    val_accuracy = history.history['val_accuracy']
-    epochs = range(1, len(accuracy) + 1)
-    plot_accuracy(epochs, accuracy,val_accuracy,fold_var)
-  
-    loss = history.history['loss']
-    val_loss = history.history['val_loss']
-    epochs = range(1, len(loss) + 1)
-    plot_loss(epochs, loss,val_loss,fold_var)
-  
-    fper, tper, thresholds = roc_curve(testLabel, y_pred)
-    plot_roc_curve(fper, tper,fold_var)
-    '''
-    tf.keras.backend.clear_session()
-    fold_var += 1
-value_min = min(scores_loss)
-value_index = scores_loss.index(value_min)
-model.load_weights(save_dir+'model_'+str(value_index)+".hdf5")
-best_model = model
-best_model.save_weights('model_weights.h5')
-best_model.save('model_keras.h5')
+    y_pred = self.best_models[0].predict([testText,testImage])
+    return y_pred
+
+def plot_learning_curve(self):
+    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(20, 1 * 6), dpi=100)
+    # Classification Report curve
+    sns.lineplot(x=np.arange(1, self.epochs + 1), y=self.best_models[0].history.history['accuracy'],palette=['b'], ax=axes[0][0],label='train_accuracy')
+    sns.lineplot(x=np.arange(1, self.epochs + 1), y=self.best_models[0].history.history['val_accuracy'],palette=['r'], ax=axes[0][0],label='val_accuracy')       
+    axes[0][0].legend()
+    # Loss curve
+    sns.lineplot(x=np.arange(1, self.epochs + 1), y=self.best_models[0].history.history['loss'],palette=['b'], ax=axes[0][1], label='train_loss')
+    sns.lineplot(x=np.arange(1, self.epochs + 1), y=self.best_models[0].history.history['val_loss'],palette=['r'], ax=axes[0][1], label='val_loss')
+    axes[0][1].legend() 
+    for j in range(2):
+        axes[0][j].set_xlabel('Epoch', size=12)
+    plt.savefig('courbes_Accuracy_loss.png')
+    plt.show()
+def confusionMatrix(self):
+    cm = confusion_matrix(testLabel, pred_labels)
+    class_names = ['fake','real']
+    #Transform to df for easier plotting
+    cm_df = pd.DataFrame(cm)
+    final_cm = cm_df
+    plt.figure(figsize = (5,5))
+    sns.heatmap(final_cm, annot = True,cmap="YlGnBu",cbar=False,fmt='d')
+    plt.title('Confusion matrix', y=1.1)
+    plt.ylabel('Actual class')
+    plt.xlabel('Prediction class')
+    plt.show()
+
+def potCurve(self,y_test,y_pred):
+    #define metrics
+    fpr, tpr, _ = metrics.roc_curve(y_test, y_pred)
+    auc = metrics.roc_auc_score(y_test, y_pred)
+    #create ROC curve
+    plt.plot(fpr,tpr,color='red',label="AUC="+str(auc))
+    plt.plot([0, 1], [0, 1], color='green')
+    plt.ylabel('True Positive Rate')
+    plt.xlabel('False Positive Rate')
+    plt.legend(loc=4)
+    plt.savefig('ROC_Curve.png')
+    plt.show()
